2f9d05b29a6b8e7334ba4c3f03dc961bafc70dad
Jenish vamja <153750818+JenishVamja73@users.noreply.github.com>
2025-01-18 10:50:40 +0000

    javascripert

---
+++ veraible.js
@@ -0,0 +1,11 @@
+let score = null
+console.log(score)
+console.log(typeof score) 
+
+let valueNumber = Number(score)
+console.log(typeof valueNumber)
+console.log(valueNumber)
+let  islenagth = 1;
+let value = Boolean(islenagth)
+console.log(typeof value,value)
+//1 true and 0 > false value boolen value
