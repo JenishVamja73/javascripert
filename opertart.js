a33f066d497be9de14a7653dfba70ab2650e0d35
Jenish vamja <153750818+JenishVamja73@users.noreply.github.com>
2025-01-19 10:19:01 +0000

    React-Native

---
+++ opertart.js
@@ -0,0 +1,9 @@
+// console.log("02">1)
+// console.log(undefined==0)
+// console.log(undefined<0)
+// console.log(undefined>0)
+// console.log("2"=== 2)
+
+const id = Symbol ("123")
+const anotherid = Symbol('123')
+console.log(id === anotherid)
